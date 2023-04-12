5b697a0fceba7fa22e2ac481c38b8b769a811a59
Your Name <chamidilshan55@gmail.com>
2023-04-12 19:50:42 +0530

    added styles for task page

--- frontend/lib/new_task.dart
+++ frontend/lib/new_task.dart
@@ -18,31 +18,78 @@ class _NewTaskPageState extends State<NewTaskPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       body: Center(
-        child: Column(
-          children: [
-            TextField(
-              controller: _taskController,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Task',
+        child: Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 50.0,
               ),
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                if(_taskController.text.isNotEmpty){
-                  Navigator.push(context, MaterialPageRoute(builder: (context)=> HomePage()));
-                  await RemoteService().addTask(_taskController.text);
-                  _taskController.clear();
-                }
-              },
-              child: Text('Add Task'),
-            ),
-          ],
-        ),
+              Text('Add a new task',
+                style: TextStyle(
+                    fontSize: 24.0,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              TextField(
+                controller: _taskController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Task',
+                  labelStyle: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 16.0
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blue, width: 2.0),
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+                style: TextStyle(
+                  fontSize: 16.0,
+                  color: Colors.black,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  if(_taskController.text.isNotEmpty){
+                    Navigator.push(context, MaterialPageRoute(builder: (context)=> HomePage()));
+                    await RemoteService().addTask(_taskController.text);
+                    _taskController.clear();
+                  }
+                },
+                child: Text('Add Task',
+                  style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                    primary: Colors.blue[900],
+                    padding: EdgeInsets.symmetric(horizontal: 50.0, vertical: 15.0),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0)
+                    )
+                ),
+              ),
+            ],
+          ),
+        ), 
       ),
     );
   }
